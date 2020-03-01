6b5b3113a6bffe50f4cd1755da0d0a3699c93b6c
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-03-01 18:07:07 -0500

    C is such a neat and dainty feeling language

--- intro/main.c
+++ intro/main.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
+// numeric types
+int a;
+long b;
+float f;
+double c;
+char g;
+
+// initialization
+int a = 12;
+float f = 32.9;
+
+// Arrays
+int x[20]; // array of 20 ints
+float y[2][20]; // 2D arrays, two rows of 20 columns of floats...wut
+
+int h[5] = {4, 7, 2, 9, 3};
+int j[2][3] = {{1, 2, 3}, {4, 5, 6}};
+int k[] = {10, 50, 100};
 int main() {
     printf("Hello, World!\n");
     return 0;
