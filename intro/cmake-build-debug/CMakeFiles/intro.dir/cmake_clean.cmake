8b753d952ec23902aef3f74d40662ee0c6d04215
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-03-01 17:53:56 -0500

    That was definitely C++

--- intro/cmake-build-debug/CMakeFiles/intro.dir/cmake_clean.cmake
+++ intro/cmake-build-debug/CMakeFiles/intro.dir/cmake_clean.cmake
@@ -1,10 +1,10 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/intro.dir/main.cpp.o"
+  "CMakeFiles/intro.dir/main.c.o"
   "intro.pdb"
   "intro"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
+foreach(lang C)
   include(CMakeFiles/intro.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
